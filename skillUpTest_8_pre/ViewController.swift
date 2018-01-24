472cb1f42f5e2a2d9fc6d473e8f127bf05018503
S N <will@kpc-014c1104-3.local>
2018-01-24 13:25:53 +0900

    first commit

---
+++ skillUpTest_8_pre/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  skillUpTest_8_pre
+//
+//  Created by S N on 2018/01/24.
+//  Copyright © 2018年 Seidi Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
